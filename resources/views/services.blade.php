4e266462cf8bc6315e6ca013ccedea532528b707
weishengit <weishenemail@gmail.com>
2022-11-16 16:08:40 +0800

    added our services page

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -32,15 +32,13 @@
         <h2 class="section-title position-relative text-uppercase mx-xl-5 mb-4"><span class="bg-secondary pr-3">Our Services</span></h2>
         <div class="row px-xl-5 mb-4">
             <div class="col-lg-5 px-0">
-                <img class="img-fluid" src="/public/iloilo-bus.webp" alt="Image">
+                <img class="img-fluid" src="/public/about-img-1.jpg" alt="Image">
             </div>
             <div class="col-lg-7 h-auto px-0">
                 <div class="h-100 bg-light p-30 d-flex flex-column justify-content-center">
-                    <h1>Tours</h1>
+                    <h1>Bus Rental</h1>
                     <h4 class="mt-4 text-muted">
-                        Volup erat ipsum diam elitr rebum et dolor. Est nonumy elitr erat diam stet sit
-                        clita ea. Sanc ipsum et, labore clita lorem magna duo dolor no sea
-                        Nonumy
+                        Jet Bus Travel and Tours offers buses for rent. It has been especially popular for tours, gatherings, and other special events.
                     </h4>
                 </div>
             </div>
@@ -48,31 +46,40 @@
         <div class="row px-xl-5 mb-4">
             <div class="col-lg-7 h-auto px-0">
                 <div class="h-100 bg-light p-30 d-flex flex-column justify-content-center">
-                    <h1>Travels</h1>
+                    <h1>Day Tours</h1>
                     <h4 class="mt-4 text-muted">
-                        Volup erat ipsum diam elitr rebum et dolor. Est nonumy elitr erat diam stet sit
-                        clita ea. Sanc ipsum et, labore clita lorem magna duo dolor no sea
-                        Nonumy
+                        Enjoy day tours with Jet Bus Travel and Tours!
                     </h4>
                 </div>
             </div>
             <div class="col-lg-5 px-0">
-                <img class="img-fluid" src="/public/iloilo-bus.webp" alt="Image">
+                <img class="img-fluid" src="/public/about-img-5.webp" alt="Image">
             </div>
         </div>
         <div class="row px-xl-5 mb-4">
             <div class="col-lg-5 px-0">
-                <img class="img-fluid" src="/public/iloilo-bus.webp" alt="Image">
+                <img class="img-fluid" src="/public/about-img-4.jpg" alt="Image">
             </div>
             <div class="col-lg-7 h-auto px-0">
                 <div class="h-100 bg-light p-30 d-flex flex-column justify-content-center">
-                    <h1>Trips</h1>
+                    <h1>Multi-day Tours</h1>
                     <h4 class="mt-4 text-muted">
-                        Volup erat ipsum diam elitr rebum et dolor. Est nonumy elitr erat diam stet sit
-                        clita ea. Sanc ipsum et, labore clita lorem magna duo dolor no sea
-                        Nonumy
+                        Two or more is better than one, Jet Bus Travel and Tours also offers multiple trips!
                     </h4>
                 </div>
+            </div>
+        </div>
+        <div class="row px-xl-5 mb-4">
+            <div class="col-lg-7 h-auto px-0">
+                <div class="h-100 bg-light p-30 d-flex flex-column justify-content-center">
+                    <h1>Tour Packages</h1>
+                    <h4 class="mt-4 text-muted">
+                        Affordable, safe, and numerous amount of tour packages created by Jet Bus Travel and Tours team!
+                    </h4>
+                </div>
+            </div>
+            <div class="col-lg-5 px-0">
+                <img class="img-fluid" src="/public/about-img-3.jpg" alt="Image">
             </div>
         </div>
     </div>
